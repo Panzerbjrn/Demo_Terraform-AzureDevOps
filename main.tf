e7ad65e29da1e9faf4aeae44006232aa3d232fc1
Lars Petersson <lars.petersson@lexacom.co.uk>
2023-12-06 21:44:07 +0000

    Refreshing

---
+++ main.tf
@@ -0,0 +1,29 @@
+#terraform {
+#  backend "azurerm" {
+#    resource_group_name  = "tfrg"
+#    storage_account_name = "tfsa"
+#    container_name       = "tfcont"
+#    key                  = "terraformgithubexample.tfstate"
+#  }
+#}
+#
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. #
+  ## If you're using version 1.x, the "features" block is not allowed. ##
+  version = "~>3.0"
+  features {}
+}
+
+#Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "TerraformGHActionsDemo"
+  location = "westeurope"
+  tags = {
+    source = "TerraformGHActions"
+  }
+}
+
+module "Webserver_Module" {
+  source = "./Modules/Webserver"
+}
