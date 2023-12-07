ff5bfa81395f01c451e34e119683d2e511e427ee
Lars Petersson <lars.petersson@lexacom.co.uk>
2023-12-07 21:52:30 +0000

    Refreshing

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name  = "tfrg"
-#    storage_account_name = "tfsa"
-#    container_name       = "tfcont"
-#    key                  = "terraformgithubexample.tfstate"
-#  }
-#}
-#
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfrg"
+    storage_account_name = "tfsa"
+    container_name       = "tfcont"
+    key                  = "terraformgithubexample.tfstate"
+  }
+*/
+}
+
 
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. #
